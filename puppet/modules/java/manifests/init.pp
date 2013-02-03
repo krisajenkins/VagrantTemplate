536228a43030de5a40e5881b2ec1b267d3b54502
Kris Jenkins <krisajenkins@gmail.com>
2013-02-03 08:43:58 +0000

    Tidying the java init.pp file.

--- puppet/modules/java/manifests/init.pp
+++ puppet/modules/java/manifests/init.pp
@@ -9,15 +9,15 @@ class java {
     require => Exec["add-java-repo"];
   }
 
-  package { "oracle-java7-installer":
-    ensure => "installed",
-    require => [File["/var/cache/debconf/java7.seeds"],
-                Exec["make-java-available"]],
-    responsefile => "/var/cache/debconf/java7.seeds";
-  }
-
   file { "/var/cache/debconf/java7.seeds":
     source => "puppet:///modules/java/java7.seeds",
     ensure => present;
+  }
+
+  package { "oracle-java7-installer":
+    require => [File["/var/cache/debconf/java7.seeds"],
+                Exec["make-java-available"]],
+    responsefile => "/var/cache/debconf/java7.seeds",
+    ensure => "installed";
   }
 }
